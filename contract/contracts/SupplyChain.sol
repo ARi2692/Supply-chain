47cf42e325e93ec91e249ceadd848ccc05acaa48
ARi2692 <arundhati.gunu@gmail.com>
2023-06-28 14:14:52 +0530

    Added: details of each stage

--- contract/contracts/SupplyChain.sol
+++ contract/contracts/SupplyChain.sol
@@ -6,15 +6,82 @@ pragma solidity >=0.8.2 <0.9.0;
  * @title Supply chain
  * @dev Supply chain system aims to monitor and trace food products
  */
-contract SupplyChain {
+contract Storage {
 
+    struct product {
+        uint256 productID;
+        string productName;
+        bool safetyCheck; 
+        string origin;
+        uint256 batchNo;
+        uint256 expiryDate;
+        uint256 totalVolume;
+        uint256 temperatureLowerLimit; // for range of temperature to be maintained
+        uint256 temperatureUpperLimit;
+        string instructions; // message instructions storagecond
+    }
+
+    struct farmer {
+        uint256 farmerID;
+        uint256 productIndex;
+        uint256 dateTimeDelivered;
+    }
+
+    // to distribution after processing
     struct Manufacturer{
+        uint256 manufacturerID;
+        uint256 productIndex;
+        uint256 dateTimeDelivered;
         uint256 temperature;
     }
 
-    // for range of temperature to be maintained
-    uint256 temperatureLowerLimit;
-    uint256 temperatureUpperLimit;
+    struct processor {
+        uint256 productIndex;
+        bool qualityCheck;
+        bool saferToConsume;
+        uint256 safeAboveAge;
+    }
+
+    struct distributionCompany {
+        uint256 distrubutorCompanyID;
+        uint256 productIndex;
+        uint256 temperature;
+        uint256 ordersReceived;
+        uint256 Volume;
+        uint256 dateTimeReceived;
+    }
+
+    struct distributionCentre {
+        uint256 distrubutorCentreID;
+        uint256 productIndex;
+        uint256 temperature;
+        uint256 ordersReceived;
+        uint256 Volume;
+        uint256 dateTimeReceived;
+    }
+
+    struct deliveryTruck {
+        uint256 deliveryTruckID;
+        uint256 productIndex;
+        uint256 temperature;
+        uint256 Volume;
+        uint256 dateTimeStartedDelivering;
+    }
+
+    struct retailer {
+        uint256 retailerID;
+        uint256 productIndex;
+        uint256 temperature;
+        uint256 Volume;
+        uint256 dateTimeReceived;
+    }
+
+    struct consumer {
+        uint256 productIndex;
+        uint256 temperature;
+        uint256 unitsReceived;
+        uint256 dateTimeReceived;
+    }
 
     /**
      * @dev Store value in variable
