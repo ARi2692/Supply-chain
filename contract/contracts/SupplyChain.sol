277117bf6ffc4aa06549c8c6052d182b4ad62479
ARi2692 <arundhati.gunu@gmail.com>
2023-07-18 18:35:33 +0530

    Modified: contract for stage

--- contract/contracts/SupplyChain.sol
+++ contract/contracts/SupplyChain.sol
@@ -12,9 +12,9 @@ contract SupplyChain {
     struct Product {
         string productName;
         string origin;
-        uint256 batchNo; // supplier
+        uint256 batchNo; // supplier will input
         uint256 harvestDate;
-        uint256 expiryDate; // supplier
+        uint256 expiryDate; // supplier will input
         uint256 totalVolume;
         uint256 idealTemperature; // for range of temperature to be maintained
         string envInfo; // message instructions storagecond
@@ -124,16 +124,21 @@ contract SupplyChain {
     // productIndex to consumer
     mapping(uint256 => Consumer) private customers;
 
+    modifier productPresent(uint256 productID) {
+        require(products.length > productID, "Product doesnot exist");
+        _;
+    }
+
     /**
      * @notice farmer inputs the details when deliveried to the supplier Company
      * @dev farmer inputs details and adds the product details
      * @param _farmerID - farmer ID
      * @param _productName - the product name
      * @param _origin - the origin place of the product
-     * @param _harvestDate - expiry date of the product
+     * @param _harvestDate - harvest date of the product
      * @param _totalVolume - total volume deliveried to the supplier
      * @param _temperatureLimit - the temperature limit below which the product should be kept
-     * @param _envInfo - any additional instructions provided by the farmer
+     * @param _envInfo - any additional instructions provided by the farmer regarding environment factors
      */
     function farmerDetails(
         uint256 _farmerID,
@@ -151,7 +156,7 @@ contract SupplyChain {
                 origin: _origin,
                 batchNo: 0,
                 harvestDate: _harvestDate,
-                expiryDate: 0, // check
+                expiryDate: 0, 
                 totalVolume: _totalVolume,
                 idealTemperature: _temperatureLimit,
                 envInfo: _envInfo,
@@ -165,14 +170,18 @@ contract SupplyChain {
     }
 
     /**
-     * @notice supplier inputs the details when deliveried to the Distribution Company
-     * @dev supplier inputs details after regulator inputs
+     * @notice supplier inputs the details after processing
+     * @dev supplier inputs details 
      * @param _supplierID - Supplier ID
      * @param _productID - the product ID
      * @param _temperature - the temperature at the time of delivery
+     * @param _specificationsAndProcessingInfo - specifications And Processing Info
+     * @param _safeAboveAge - safe above the age 
+     * @param _batchNo - the batch number of the product
+     * @param _expiryDate - the expiry date
+     * @param _isOrganic - is organic
      */
-    //  change supplier to Supplier
-    function processorAndsupplierDetails(
+    function processorAndSupplierDetails(
         uint256 _supplierID,
         uint256 _productID,
         uint256 _temperature,
@@ -181,8 +190,8 @@ contract SupplyChain {
         uint256 _batchNo,
         uint256 _expiryDate,
         bool _isOrganic
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
+    ) external productPresent(_productID)  {
+        require(products[_productID].stage == 0, "Product not yet authorized for processing");
         suppliers[_productID] = Supplier({
             supplierID: _supplierID,
             dateTimeDelivered: block.timestamp,
@@ -197,12 +206,13 @@ contract SupplyChain {
     }
 
     /**
-     * @notice regulator inputs the details when deliveried to the supplier Company
-     * @dev regulator inputs details before supplier
+     * @notice regulator inputs the details after supplier
+     * @dev regulator inputs details
      * @param _productID - product ID
-     * @param _regulatorID - true if the quality check is passed and cleared
-     * @param _permitRequirementsFulfilled - true if certified as safer to consume
-     * @param _sanctionsImposed - the age above which it is safe to consume
+     * @param _regulatorID - regulator ID
+     * @param _permitRequirementsFulfilled - permit Requirements Fulfilled
+     * @param _sanctionsImposed - sanctions Imposed
+     * @param _analysisInfo - analysis Info
      */
     function regulatorDetails(
         uint256 _productID,
@@ -210,8 +220,8 @@ contract SupplyChain {
         bool _permitRequirementsFulfilled,
         bool _sanctionsImposed,
         string memory _analysisInfo
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
+    ) external productPresent(_productID) {
+        require(products[_productID].stage == 1, "Product not for regulator");
         regulators[_productID] = Regulator({
             regulatorID: _regulatorID,
             permitRequirementsFulfilled: _permitRequirementsFulfilled,
@@ -222,13 +232,17 @@ contract SupplyChain {
     }
 
     /**
-     * @notice qualityAssuranceAnalyst inputs the details when deliveried to the supplier Company
-     * @dev qualityAssuranceAnalyst inputs details before supplier
+     * @notice qualityAssuranceAnalyst inputs the details after regulator
+     * @dev qualityAssuranceAnalyst inputs details 
      * @param _productID - product ID
-     * @param _assuranceID - true if the quality check is passed and cleared
-     * @param _qualityStandardsMeet - true if certified as safer to consume
-     * @param _audited - the age above which it is safe to consume
-     * @param _verified - the age above which it is safe to consume
+     * @param _assuranceID - assurance ID
+     * @param _qualityStandardsMeet - _quality Standards Meet
+     * @param _audited - audited or not?
+     * @param _verified - verified or not?
+     * @param _guidelinesMeet - guidelines Meet or not?
+     * @param _compliant - compliant or not?
+     * @param _certifyingbodyID - certifyingbody ID 
+     * @param _certificationInfo - certification Info 
      */
     function qualityAssuranceAnalystDetails(
         uint256 _productID,
@@ -240,8 +254,8 @@ contract SupplyChain {
         bool _compliant,
         uint256 _certifyingbodyID,
         string memory _certificationInfo
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
+    ) external productPresent(_productID) {
+        require(products[_productID].stage == 2, "Product not for quality assurance");
         qualityAssuranceAnalysts[_productID] = QualityAssurance({
             assuranceID: _assuranceID,
             qualityStandardsMeet: _qualityStandardsMeet,
@@ -256,13 +270,15 @@ contract SupplyChain {
     }
 
     /**
-     * @notice distributor inputs the details
+     * @notice distributor inputs the details after quality assurance
      * @dev distributor inputs details when it received
      * @param _distributorID - distrubutor Company ID
      * @param _productID - the product ID
      * @param _temperature - the temperature at the time of delivery
      * @param _ordersReceived - number of orders received
      * @param _volume - the volume it received
+     * @param _warehouseFacilities - warehouse Facilities
+     * @param _productHandlingInfo - product Handling Info
      */
     function distributorDetails(
         uint256 _distributorID,
@@ -272,8 +288,8 @@ contract SupplyChain {
         uint256 _volume,
         string memory _warehouseFacilities,
         string memory _productHandlingInfo
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
+    ) external productPresent(_productID) {
+        require(products[_productID].stage == 3, "Product not for distributor");
         distributors[_productID] = Distributor({
             distributorID: _distributorID,
             temperature: _temperature,
@@ -289,9 +305,10 @@ contract SupplyChain {
     /**
      * @notice delivery Truck / logistics inputs the details
      * @dev delivery Truck / logistics inputs details when Delivery starts
-     * @param _logisticsID - delivery Truck ID
+     * @param _logisticsID - delivery Truck / logistics ID
      * @param _productID - the product ID
      * @param _temperature - the temperature when started with delivery
+     * @param _modeOfTransport - mode Of Transport 0-road, 1-train, 2-water, 3-air
      * @param _volume - the volume it received
      */
     function logisticsDetails(
@@ -300,8 +317,8 @@ contract SupplyChain {
         uint256 _temperature,
         uint256 _modeOfTransport,
         uint256 _volume
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
+    ) external productPresent(_productID) {
+        require(products[_productID].stage == 4, "Product not for logistics");
         logistics[_productID] = Logistics({
             logisticsID: _logisticsID,
             temperature: _temperature,
@@ -319,6 +336,9 @@ contract SupplyChain {
      * @param _productID - the product ID
      * @param _temperature - the temperature when received
      * @param _volume - the volume it received
+     * @param _complianceInfo - compliance Info
+     * @param _promotionalInfo - promotional Info
+     * @param _inventoryInfo - inventory Info
      */
     function retailerDetails(
         uint256 _retailerID,
@@ -328,8 +348,8 @@ contract SupplyChain {
         string memory _complianceInfo,
         string memory _promotionalInfo,
         string memory _inventoryInfo
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
+    ) external productPresent(_productID) {
+        require(products[_productID].stage == 5, "Product not for retailer");
         retailers[_productID] = Retailer({
             retailerID: _retailerID,
             temperature: _temperature,
@@ -346,6 +366,7 @@ contract SupplyChain {
      * @notice consumer inputs the details
      * @dev consumer inputs details when received
      * @param _productID - the product ID
+     * @param _consumerID - the consumer ID
      * @param _unitsReceived - units it received
      * @param _temperature - the temperature when received
      * @param _satisfied - true if satisfied with the product
@@ -356,9 +377,8 @@ contract SupplyChain {
         uint256 _unitsReceived,
         uint256 _temperature,
         bool _satisfied
-    ) external {
-        require(products.length > _productID, "Product doesnot exist");
-        // or can be used as bool satsfied with the product condition
+    ) external productPresent(_productID) {
+        require(products[_productID].stage == 6, "Product not for consumer yet!");
         customers[_productID].consumerID = _consumerID;
         if (
             _temperature < products[_productID].idealTemperature &&
@@ -372,69 +392,57 @@ contract SupplyChain {
     }
 
     // all getters functions
-
-
     function getProduct(
         uint256 _productID
-    ) external view returns (Product memory productDetails) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Product memory productDetails) {
         productDetails = products[_productID];
     }
 
     function getFarmer(
         uint256 _productID
-    ) external view returns (Farmer memory farmer) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Farmer memory farmer) {
         farmer = farmers[_productID];
     }
 
     function getSupplier(
         uint256 _productID
-    ) external view returns (Supplier memory supplier) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Supplier memory supplier) {
         supplier = suppliers[_productID];
     }
 
     function getRegulator(
         uint256 _productID
-    ) external view returns (Regulator memory regulator) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Regulator memory regulator) {
         regulator = regulators[_productID];
     }
 
     function getQualityAssurance(
         uint256 _productID
-    ) external view returns (QualityAssurance memory qualityAssurance) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (QualityAssurance memory qualityAssurance) {
         qualityAssurance = qualityAssuranceAnalysts[_productID];
     }
 
     function getDistributor(
         uint256 _productID
-    ) external view returns (Distributor memory distributor) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Distributor memory distributor) {
         distributor = distributors[_productID];
     }
 
     function getLogistics(
         uint256 _productID
-    ) external view returns (Logistics memory trackLogistics) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Logistics memory trackLogistics) {
         trackLogistics = logistics[_productID];
     }
 
     function getRetailer(
         uint256 _productID
-    ) external view returns (Retailer memory retailer) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Retailer memory retailer) {
         retailer = retailers[_productID];
     }
 
     function getConsumer(
         uint256 _productID
-    ) external view returns (Consumer memory consumer) {
-        require(products.length > _productID, "Product doesnot exist");
+    ) productPresent(_productID) external view returns (Consumer memory consumer) {
         consumer = customers[_productID];
     }
-
 }
