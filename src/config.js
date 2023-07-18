17ceb858467caf5e70d20c23c4a87a094552749e
ARi2692 <arundhati.gunu@gmail.com>
2023-07-18 18:39:41 +0530

    Added: new contract

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const networkConfig = {
     "80001": [
         {
-            supplyChainAddress: "0xfE5E29398c4Ee928DE5662A5687F6EA38BDcF401", 
+            supplyChainAddress: "0x50b546246c07BC16b6E79F5c9122e241EC20B3c1", 
             token_icon: "https://cryptologos.cc/logos/polygon-matic-logo.svg?v=022",
             alt: "MATIC",
             networkName: "Mumbai"
